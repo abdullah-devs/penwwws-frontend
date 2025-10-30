30bdc66c66e28dae5312662b146f1f1bac928e77
Ako-Mawlood <ako.mawlood01@gmail.com>
2025-10-30 08:41:36 +0300

    fix: progress bar animation duration

--- providers/ProgressBarProvider.tsx
+++ providers/ProgressBarProvider.tsx
@@ -3,8 +3,7 @@
 import { usePathname, useSearchParams } from "next/navigation";
 import { useEffect } from "react";
 import NProgress from "nprogress";
-import "nprogress/nprogress.css"; // Import styles
-
+import "nprogress/nprogress.css";
 type Props = {
   children: React.ReactNode;
 };
@@ -14,12 +13,12 @@ export default function NProgressProvider({ children }: Props) {
   const searchParams = useSearchParams();
 
   useEffect(() => {
-    NProgress.configure({ showSpinner: false, speed: 400 });
+    NProgress.configure({ showSpinner: false, speed: 300 });
     NProgress.start();
 
     const timer = setTimeout(() => {
       NProgress.done();
-    }, 500);
+    }, 300);
 
     return () => clearTimeout(timer);
   }, [pathname, searchParams]);
