209e0605d547f7d111c4933d0dc4a6f0bec32724
Ako-Mawlood <ako.mawlood01@gmail.com>
2025-10-31 12:09:58 +0300

    feat: add status palceholder component for assignment tab

--- components/features/subject/tabs/assignments/AssignmentTabContent.tsx
+++ components/features/subject/tabs/assignments/AssignmentTabContent.tsx
@@ -1,16 +1,21 @@
 import AddAssignment from "@/components/features/subject/tabs/documents/AddAssignment";
+import { StudentAssignmentCard } from "./StudentAssignmentCard";
+import { TeacherAssignmentCard } from "./TeacherAssignmentCard";
+import { NotebookText } from "lucide-react";
+
 import { getCookie } from "cookies-next";
 import { cookies } from "next/headers";
 import axios from "@/lib/axiosInstance";
 import { SchoolUserType } from "@/types/SchoolUser";
-import AssignmentsList from "./AssignmentsList";
 import { AssignmentType } from "@/types/AssignmentType";
+import { StatusPlaceholder } from "@/components/shared/StatusPlaceholder";
 
 type Props = {
   schoolId: string;
   subjectId: number;
   user: SchoolUserType;
 };
+
 async function getAssignments(schoolId: string, subjectId: number) {
   const token = await getCookie("token", { cookies });
   const res = await axios.get(
@@ -21,29 +26,65 @@ async function getAssignments(schoolId: string, subjectId: number) {
   );
   return res.data;
 }
+
 export default async function AssignmentTabContent({
   schoolId,
   subjectId,
   user,
 }: Props) {
-  const assignments: AssignmentType[] = await getAssignments(
-    schoolId,
-    subjectId,
-  );
+  let isError = false;
+  let assignments: AssignmentType[] = [];
+  try {
+    assignments = await getAssignments(schoolId, subjectId);
+  } catch {
+    isError = true;
+  }
+
   return (
-    <div>
-      <div className="mt-5 flex w-full items-center justify-between">
-        <h1 className="text-3xl font-bold">Assignments</h1>
-        {user.role !== "STUDENT" && (
+    <div className="relative min-h-[65vh]">
+      <div className="relative mt-5 flex w-full items-center justify-between">
+        <h1 className="text-primary text-3xl font-bold">Assignments</h1>
+        {user.role !== "STUDENT" && assignments.length !== 0 && (
           <AddAssignment schoolId={schoolId} subjectId={subjectId} />
         )}
       </div>
-      <AssignmentsList
-        assignments={assignments}
-        schoolId={schoolId}
-        subjectId={subjectId}
-        user={user}
-      />
+
+      {isError && (
+        <StatusPlaceholder
+          title="Couldn't Fetch Assignments"
+          description="Something went wrong and we could not get the assignments, please refresh or try again later."
+          icon={<NotebookText />}
+          variant="destructive"
+        />
+      )}
+      {assignments.length == 0 && !isError ? (
+        <StatusPlaceholder
+          title="No subjects yet"
+          description="Currently there is not assignment. Click new assignment to create one."
+          action={<AddAssignment schoolId={schoolId} subjectId={subjectId} />}
+          icon={<NotebookText />}
+        />
+      ) : (
+        <section className="relative mt-6 grid w-full grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4">
+          {assignments.map((assignment) =>
+            user.role !== "STUDENT" ? (
+              <TeacherAssignmentCard
+                key={assignment.id}
+                schoolId={schoolId}
+                subjectId={subjectId}
+                assignment={assignment}
+              />
+            ) : (
+              <StudentAssignmentCard
+                key={assignment.id}
+                schoolId={schoolId}
+                subjectId={subjectId}
+                assignment={assignment}
+              />
+            ),
+          )}
+        </section>
+      )}
     </div>
   );
 }
